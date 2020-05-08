007284482188846a88b36b9debbbd41477d95d3a
smgarner229 <smgarner@berkeley.edu>
2020-05-07 20:53:06 -0700

    Learning how to construct a structure

--- cpp_testing/main.cpp
+++ cpp_testing/main.cpp
@@ -4,10 +4,52 @@
 #include <stdio.h>      /* printf, scanf, NULL */
 #include <stdlib.h>     /* malloc, free, rand */
 
+struct Rectangle{
+    int w,h;
+    
+    Rectangle()
+    {
+        w=1;
+        h=1;
+    }
+    Rectangle(int h1)
+    {
+        w=1;
+        h=h1;
+    }
+    Rectangle(int h1, int w1)
+    {
+        w=w1;
+        h=h1;
+    }
+    int get_w()
+    {
+        return w;
+    }
+    int get_h()
+    {
+        return h;
+    }
+};
+
 int main ()
 {
   int i,n;
   char * buffer;
+
+  Rectangle tl(5,5);
+  std::cout << tl.w << " " << tl.h << std::endl;
+
+  Rectangle * sqp;
+  sqp = new Rectangle();
+  std::cout << sqp->get_w() << " " << sqp->get_h() << std::endl;
+  delete sqp;
+
+//  Rectangle sqp;
+//  std::cout<<sqp.w<<" "<<sqp.h<<std::endl;
+  
+  Rectangle ol(5);
+  std::cout << ol.get_w() << " " << ol.h << std::endl;
 
   printf ("How long do you want the string? ");
 //  scanf ("%d", &i);
